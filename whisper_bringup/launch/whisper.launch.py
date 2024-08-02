f8d32b4fa36bae5e00bb1ee149ec30017e1a6bda
Miguel Ángel González Santamarta <mgons@unileon.es>
2024-08-02 15:30:22 +0200

    stream param and spanish stream example

--- whisper_bringup/launch/whisper.launch.py
+++ whisper_bringup/launch/whisper.launch.py
@@ -26,7 +26,7 @@ from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch.actions import OpaqueFunction, DeclareLaunchArgument
 from huggingface_hub import hf_hub_download
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 
 
 def generate_launch_description():
@@ -40,64 +40,81 @@ def generate_launch_description():
             model_path = hf_hub_download(
                 repo_id=repo, filename=file, force_download=False)
 
+        params = {
+            "sampling_strategy": LaunchConfiguration("sampling_strategy", default="beam_search"),
+            "model": LaunchConfiguration("model", default=model_path),
+            "openvino_encode_device": LaunchConfiguration("openvino_encode_device", default="CPU"),
+
+            "n_threads": LaunchConfiguration("n_threads", default=4),
+            "n_max_text_ctx": LaunchConfiguration("n_max_text_ctx", default=16384),
+            "offset_ms": LaunchConfiguration("offset_ms", default=0),
+            "duration_ms": LaunchConfiguration("duration_ms", default=0),
+
+            "translate": LaunchConfiguration("translate", default=False),
+            "no_context": LaunchConfiguration("no_context", default=True),
+            "no_timestamps": LaunchConfiguration("no_timestamps", default=False),
+            "single_segment": LaunchConfiguration("single_segment", default=True),
+            "print_special": LaunchConfiguration("print_special", default=False),
+            "print_progress": LaunchConfiguration("print_progress", default=False),
+            "print_realtime": LaunchConfiguration("print_realtime", default=False),
+            "print_timestamps": LaunchConfiguration("print_timestamps", default=False),
+
+            "token_timestamps": LaunchConfiguration("token_timestamps", default=False),
+            "thold_pt": LaunchConfiguration("thold_pt", default=0.01),
+            "thold_ptsum": LaunchConfiguration("thold_ptsum", default=0.01),
+            "max_len": LaunchConfiguration("max_len", default=0),
+            "split_on_word": LaunchConfiguration("split_on_word", default=False),
+            "max_tokens": LaunchConfiguration("max_tokens", default=0),
+
+            "audio_ctx": LaunchConfiguration("audio_ctx", default=0),
+            "tinydiarize": LaunchConfiguration("tinydiarize", default=False),
+
+            "language": LaunchConfiguration("language", default="en"),
+            "detect_language": LaunchConfiguration("detect_language", default=False),
+
+            "suppress_blank": LaunchConfiguration("suppress_blank", default=True),
+            "suppress_non_speech_tokens": LaunchConfiguration("suppress_non_speech_tokens", default=False),
+
+            "temperature": LaunchConfiguration("temperature", default=0.00),
+            "max_initial_ts": LaunchConfiguration("max_initial_ts", default=1.00),
+            "length_penalty": LaunchConfiguration("length_penalty", default=-1.00),
+
+            "temperature_inc": LaunchConfiguration("temperature_inc", default=0.40),
+            "entropy_thold": LaunchConfiguration("entropy_thold", default=2.40),
+            "logprob_thold": LaunchConfiguration("logprob_thold", default=-1.00),
+            "no_speech_thold": LaunchConfiguration("no_speech_thold", default=0.60),
+
+            "greedy_best_of": LaunchConfiguration("greedy_best_of", default=5),
+            "beam_search_beam_size": LaunchConfiguration("beam_search_beam_size", default=5),
+            "beam_search_patience": LaunchConfiguration("beam_search_patience", default=-1.00),
+
+            "n_processors": LaunchConfiguration("n_processors", default=1),
+            "use_gpu": LaunchConfiguration("use_gpu", default=True),
+            "gpu_device": LaunchConfiguration("gpu_device", default=0),
+        }
+
         return Node(
             package="whisper_ros",
             executable="whisper_server_node",
             name="whisper_node",
             namespace="whisper",
-            parameters=[{
-                "sampling_strategy": LaunchConfiguration("sampling_strategy", default="beam_search"),
-                "model": LaunchConfiguration("model", default=model_path),
-                "openvino_encode_device": LaunchConfiguration("openvino_encode_device", default="CPU"),
-
-                "n_threads": LaunchConfiguration("n_threads", default=4),
-                "n_max_text_ctx": LaunchConfiguration("n_max_text_ctx", default=16384),
-                "offset_ms": LaunchConfiguration("offset_ms", default=0),
-                "duration_ms": LaunchConfiguration("duration_ms", default=0),
-
-                "translate": LaunchConfiguration("translate", default=False),
-                "no_context": LaunchConfiguration("no_context", default=True),
-                "no_timestamps": LaunchConfiguration("no_timestamps", default=False),
-                "single_segment": LaunchConfiguration("single_segment", default=True),
-                "print_special": LaunchConfiguration("print_special", default=False),
-                "print_progress": LaunchConfiguration("print_progress", default=False),
-                "print_realtime": LaunchConfiguration("print_realtime", default=False),
-                "print_timestamps": LaunchConfiguration("print_timestamps", default=False),
-
-                "token_timestamps": LaunchConfiguration("token_timestamps", default=False),
-                "thold_pt": LaunchConfiguration("thold_pt", default=0.01),
-                "thold_ptsum": LaunchConfiguration("thold_ptsum", default=0.01),
-                "max_len": LaunchConfiguration("max_len", default=0),
-                "split_on_word": LaunchConfiguration("split_on_word", default=False),
-                "max_tokens": LaunchConfiguration("max_tokens", default=0),
-
-                "audio_ctx": LaunchConfiguration("audio_ctx", default=0),
-                "tinydiarize": LaunchConfiguration("tinydiarize", default=False),
-
-                "language": LaunchConfiguration("language", default="en"),
-                "detect_language": LaunchConfiguration("detect_language", default=False),
-
-                "suppress_blank": LaunchConfiguration("suppress_blank", default=True),
-                "suppress_non_speech_tokens": LaunchConfiguration("suppress_non_speech_tokens", default=False),
-
-                "temperature": LaunchConfiguration("temperature", default=0.00),
-                "max_initial_ts": LaunchConfiguration("max_initial_ts", default=1.00),
-                "length_penalty": LaunchConfiguration("length_penalty", default=-1.00),
-
-                "temperature_inc": LaunchConfiguration("temperature_inc", default=0.40),
-                "entropy_thold": LaunchConfiguration("entropy_thold", default=2.40),
-                "logprob_thold": LaunchConfiguration("logprob_thold", default=-1.00),
-                "no_speech_thold": LaunchConfiguration("no_speech_thold", default=0.60),
-
-                "greedy_best_of": LaunchConfiguration("greedy_best_of", default=5),
-                "beam_search_beam_size": LaunchConfiguration("beam_search_beam_size", default=5),
-                "beam_search_patience": LaunchConfiguration("beam_search_patience", default=-1.00),
-
-                "n_processors": LaunchConfiguration("n_processors", default=1),
-                "use_gpu": LaunchConfiguration("use_gpu", default=True),
-                "gpu_device": LaunchConfiguration("gpu_device", default=0),
-            }]
+            parameters=[params],
+            condition=UnlessCondition(PythonExpression(
+                [LaunchConfiguration("stream")]))
+        ), Node(
+            package="whisper_ros",
+            executable="whisper_node",
+            name="whisper_node",
+            namespace="whisper",
+            parameters=[params],
+            condition=IfCondition(PythonExpression(
+                [LaunchConfiguration("stream")]))
         ),
+
+    stream_cmd = DeclareLaunchArgument(
+        "stream",
+        default_value="False",
+        description="Whether to launch stream or server node")
 
     model_repo = LaunchConfiguration("model_repo")
     model_repo_cmd = DeclareLaunchArgument(
@@ -118,6 +135,7 @@ def generate_launch_description():
         description="Local path to the model file")
 
     return LaunchDescription([
+        stream_cmd,
         model_repo_cmd,
         model_filename_cmd,
         model_path_cmd,
@@ -130,7 +148,8 @@ def generate_launch_description():
             name="silero_vad_node",
             namespace="whisper",
             parameters=[{
-                "enabled": LaunchConfiguration("vad_enabled", default=False),
+                "enabled": LaunchConfiguration("vad_enabled",
+                                               default=PythonExpression([LaunchConfiguration("stream")])),
                 "threshold": LaunchConfiguration("vad_threshold", default=0.5),
             }],
             remappings=[("audio", "/audio/in")]
